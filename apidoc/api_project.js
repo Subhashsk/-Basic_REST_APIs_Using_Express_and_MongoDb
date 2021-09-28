afdb0a13aa46aa689910dfb4ff88f2ba1c9a94f6
Subhash Kuril <subhashskdranzer2411@gmail.com>
2021-09-28 21:35:58 +0530

    first commit

---
+++ apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Ecommerce Application",
+  "version": "0.0.1",
+  "description": "Documentation for the ecommerce web application",
+  "title": "Ecommerce APIs",
+  "url": "http://localhost:3000",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-09-28T16:00:59.228Z",
+    "url": "https://apidocjs.com",
+    "version": "0.29.0"
+  }
+});
